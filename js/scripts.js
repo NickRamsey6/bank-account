81d8a83ddb7353d8541f399504f5d67c65aa565a
Alex Sasser and Nick Ramsey <nick@>
2019-03-18 20:50:33 -0700

    make it close to resolving objects needed for new account balance.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,36 @@
 // business logic
-// var pingOrPong = function(number) {
-//   for (var i = 1; i <= number; i ++) {
-//     if (i % 15 === 0) {
-//       $(".list").append("<li>" + "pingpong" + "</li>");
-//     } else if (i % 5 === 0) {
-//       $(".list").append("<li>" + "pong" + "</li>");
-//     } else if (i % 3 === 0) {
-//       $(".list").append("<li>" + "ping" + "</li>");
-//     } else {
-//       $(".list").append("<li>" + (i) + "</li>");
-//     }
-//   }
-// };
-var add = function(initial, deposit) {
-  return initial + deposit;
-}
-var subtract = function(initial, withdrawal) {
-  return initial - withdrawal;
-}
+function Account(balance) {
+  this.balance = balance;
+};
+
+Account.prototype.Deposit = function () {
+  return this.balance(balance + deposit);
+};
+
+Account.prototype.Withdrawal = function () {
+  return this.balance (balance - withdrawal);
+};
 
 
 // user interface logic
 $(document).ready(function() {
-  event.preventDefault();
-  var initial = parseInt($("input#initialInput").val());
-  var deposit = parseInt($("input#depositInput").val());
-  var withdrawal = parseInt($("input#withdrawalInput").val());
-  // $("form#ping-pong").submit(function(event) {
-  //   event.preventDefault();
-  //   $(".list").empty();
-  //   var number = parseInt($("input#number").val());
-  //   var outputList = pingOrPong(number)
-  //     $("#yourNumbers").show();
-  // $("form#ping-pong").get(0).reset();
+  $("form#register").submit(function() {
+    event.preventDefault();
+    debugger;
+    var balance = parseInt($("input#initialInput").val());
+    var userAccount = new Account (balance);
+  });
+  console.log(userAccount.balance);
+  $("form#depositWithdraw").submit(function() {
+    debugger;
+    event.preventDefault();
+    var deposit = parseInt($("input#depositInput").val());
+    var withdrawal = parseInt($("input#withdrawalInput").val());
+    if (deposit === ("" || 0)) {
+      userAccount.Withdrawal();
+    } else {
+      userAccount.Deposit();
+    }
+    alert(userAccount.balance);
   });
 });
